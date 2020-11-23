60c379d1f20568a219e25bb307cb13728fffe598
beatific-queue-4949 <zivkovicmilan1987@gmail.com>
2020-11-23 16:44:41 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,6 +1,7 @@
 def square_array(array)
   # Use an Enumerable to square every element in the passed in array
   # Return a new array of the results
+  array.map {|item|item * item}
 end
 
 def summon_captain_planet(planeteer_calls)
