373ab9afa4e0c1497c1df1f82ea2210c64f7f31d
beatific-queue-4949 <zivkovicmilan1987@gmail.com>
2020-11-23 16:43:05 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -7,7 +7,7 @@ def summon_captain_planet(planeteer_calls)
   # Use an Enumerable to capitalize and add '!' to every element in the passed in array
   # Return a new array of the results
   
-  planeteer_calls.map {|item| item.}
+  planeteer_calls.map {|item| item.upcase + "!"}
 end
 
 def long_planeteer_calls(planeteer_calls)
